dae812d3045fec89f867e4dfa9115e0419f58135
xXEKLEKTOSXx <braianek777@gmail.com>
2023-10-03 11:26:12 -0300

    Operadores lógicos

---
+++ src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+        System.out.println("fim");
+    }
+    
+}
